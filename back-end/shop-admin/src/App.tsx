9f3a0197587dd8ee1bc3609f0bf9f8d31d21c1ae
mmdshon <m.h.s.m9876@gmail.com>
2024-04-15 00:03:33 +0330

    Categories list,show,edit,create in react admin added

--- back-end/shop-admin/src/App.tsx
+++ back-end/shop-admin/src/App.tsx
@@ -15,6 +15,8 @@ import { Order_details_list } from "./resource db/orderdetails";
 import { OrderDetails_Creat } from "./resource db/OrderDetails_Create";
 import { Payment_list } from "./resource db/payments";
 import { Payment_Creat } from "./resource db/PaymentCreate";
+import { CategoryList } from "./resource db/category";
+import { CategoryCreat } from "./resource db/categoryCreat";
 
 export const App = () => (
   <Admin dataProvider={dataProvider} authProvider={authProvider}>
@@ -45,6 +47,13 @@ export const App = () => (
       edit={EditGuesser}
       show={ShowGuesser}
       create={Payment_Creat}
+    />
+    <Resource
+      name="Categories"
+      list={CategoryList}
+      edit={EditGuesser}
+      show={ShowGuesser}
+      create={CategoryCreat}
     />
   </Admin>
 );
